d8bf8d7266bf57bb6ca9a12b73ebd53ef98b0259
Mikołaj <32310424+MikolajKorbanek@users.noreply.github.com>
2022-12-11 21:38:24 +0100

    Data Migrations (#5)

---
+++ src/backend/NewsletterOrganizer/NewsletterOrganizer.API/Migrations/20221211203240_Init.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsletterOrganizer.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    LanguageKey = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.LanguageKey);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsletterWords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LanguageKey = table.Column<string>(type: "TEXT", nullable: false),
+                    Word = table.Column<string>(type: "TEXT", nullable: false),
+                    LanguageKey1 = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsletterWords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NewsletterWords_Languages_LanguageKey1",
+                        column: x => x.LanguageKey1,
+                        principalTable: "Languages",
+                        principalColumn: "LanguageKey");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmailAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EmailAddress = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailAccounts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailAccounts_UserId",
+                table: "EmailAccounts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsletterWords_LanguageKey1",
+                table: "NewsletterWords",
+                column: "LanguageKey1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailAccounts");
+
+            migrationBuilder.DropTable(
+                name: "NewsletterWords");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
--- src/backend/NewsletterOrganizer/NewsletterOrganizer.API/Program.cs
+++ src/backend/NewsletterOrganizer/NewsletterOrganizer.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NewsletterOrganizer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DatabaseContext>(r => r.UseSqlite("Data Source=NewsletterOrganizer.db", b => b.MigrationsAssembly("NewsletterOrganizer.API")));
 
 var app = builder.Build();
 
